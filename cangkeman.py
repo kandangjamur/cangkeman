d7bc302427f59140903b6c023f56562ab7875e02
Robertus Hutagalung <robertus@mindtalk.com>
2014-10-15 17:36:52 +0700

    add cangke.me source

---
+++ cangkeman.py
@@ -0,0 +1,30 @@
+import webapp2
+import random
+import jinja2
+import os
+import json
+
+with open('names.json') as namefile:
+    names =  json.load(namefile) + ['BRObama', 'BRObadiah', 'BROmaha', "you look BROverworked", 'BROtini',
+                                    'BROzilla', 'BROtylicious', 'BROmageddon', 'BROprah Winfrey',
+                                    'Angelina BROlie', "Shaquille BRO'Neal", 'RonaldinBRO', 'BROhammad Ali' ]
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'name':random.choice(names)
+        }
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
+
+class Cangkem(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('cangkem.html')
+        self.response.out.write(template.render())
+
+app = webapp2.WSGIApplication([('/', MainPage),('/cangkem', Cangkem)],
+                              debug=True)
+
